089463038433832eb8d1d0d8575eff008e7ed916
Fredrik Skold <fredrik.skold@gmail.com>
2023-07-30 21:08:03 +0200

    update main read cards

--- textmode_ubongo.py
+++ textmode_ubongo.py
@@ -1,9 +1,13 @@
+import CardTemplateReader
 from Fichas import readTextFile
 
 
 def main2():
     pieces = readTextFile()
     print(f"Number of pieces: {len(pieces)}")
+
+    card_reader = CardTemplateReader.CardTemplateReader()
+    cards = card_reader.ReadFile()
 
 
 if "__main__" == __name__:
